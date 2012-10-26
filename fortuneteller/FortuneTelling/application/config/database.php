75534bb008ed2add495efeb946d845a9db8afedf
Yuki Minei <mine1mobile@gmail.com>
2012-10-26 17:21:31 +0900

    いじったけどだめなやつだった

--- fortuneteller/FortuneTelling/application/config/database.php
+++ fortuneteller/FortuneTelling/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'mecab';
+$db['default']['password'] = 'hastin1234';
+$db['default']['database'] = 'mecab_hastin';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
