54e329757b3d9909f7786f8e44cd9b22cd678084
masaya <t09090mu@sfc.keio.ac.jp>
2012-11-16 16:21:36 +0900

    fix

--- Rubyapp/main.rb
+++ Rubyapp/main.rb
@@ -4,11 +4,25 @@ require 'haml'
 require 'rubygems'
 require 'sinatra'
 
+get '/question' do
 
-get '/result' do
+  @q1 = "どちらが好きですか？"
+  @a1 = "mountain"
+  @a2 = "ocean"
 
+  haml :question
+
+end
+
+
+post '/result' do
+
+  
   @type= "冷静沈着"
   @explain= "常に冷静で物事を俯瞰しているあなたはまさに冷静沈着と言えるでしょう。"
+
+  @type= params[:first]
+  @explain= params[:first]
 
   haml :result
 
