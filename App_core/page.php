9a6aac7597b3c1974a9488566e6725e9d49a90da
Yuki Minei <mine1mobile@gmail.com>
2012-12-07 17:28:46 +0900

    いろいろ

--- App_core/page.php
+++ App_core/page.php
@@ -21,7 +21,9 @@ if($fh = @fopen('data.txt','r')){
 <style type="text/css">
 body{
 	color:<?php echo $arr['body_txt_color']; ?>;
+	/*
 	background: url(images/bg_img.jpg) repeat 50% 50% <?php echo $arr['body_bg_color']; ?>;
+	*/
 }
 a.btn,
 input[type="submit"].btn {
@@ -53,12 +55,12 @@ if (isset($_POST['signed_request'])) {
 <?php if ($data && $data['page']['liked']) : ?>
 <!-- ■ いいね後 ■ -->
 
-<a href="https://secure2188.sakura.ne.jp/mineiyuki.com/hastin/page.php" class="btn">アプリへ移動</a>
+<a href="https://secure2188.sakura.ne.jp/mineiyuki.com/hastin/" class="btn">アプリへ移動</a>
 
 <?php else : ?>
 <!-- ■ いいね前 ■ -->
 
-<h2><?php echo $arr['iine_req']; ?></h2>
+<h2><?php /* echo $arr['iine_req']; */ ?>いいねを押してね！</h2>
 
 <?php endif; ?>
 
